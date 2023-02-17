3dc3694cf2378cfc804f1f417e333d73397d4588
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-02-17 19:37:32 +0530

    Setting Up Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello from index.js');
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApUNPNrr-NiTc0hl3C-60cw7knfu0sQD0",
+    authDomain: "something-unique-9b932.firebaseapp.com",
+    projectId: "something-unique-9b932",
+    storageBucket: "something-unique-9b932.appspot.com",
+    messagingSenderId: "749115447002",
+    appId: "1:749115447002:web:39a8954d760c46756977e3",
+    measurementId: "G-Q9RD61ET2Y"
+};
+
+initializeApp(firebaseConfig);
